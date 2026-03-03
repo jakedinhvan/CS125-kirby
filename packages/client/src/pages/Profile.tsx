09ff0c324d2c8cd7103f3d87aeac91b5ccfd56a5
Jeremiah <40682211+fiveminus1@users.noreply.github.com>
2026-03-02 21:09:22 -0800

    allow user to input liked genres

--- packages/client/src/pages/Profile.tsx
+++ packages/client/src/pages/Profile.tsx
@@ -5,12 +5,60 @@ import { useEffect, useState } from "react";
 
 export default function Profile() {
   const [genres, setGenres] = useState<Genre[]>([]);
+  const [likedGenres, setLikedGenres] = useState<Genre[]>([]);
+
 
   useEffect(() => {
     axios.get("/api/kirby/genres").then((res) => {
       setGenres(res.data);
     })
   }, []);
+
+  useEffect(() => {
+    fetch("/api/kirby/liked-genres")
+      .then(res => res.json())
+      .then((likedIds: number[]) => {
+        const likedObjects = genres.filter(g =>
+          likedIds.includes(g.id)
+        );
+        setLikedGenres(likedObjects);
+      });
+}, [genres]);
+
+  const handleChange = async (
+    event: any,
+    newValue: Genre[],
+  ) => {
+    const added = newValue.filter(
+      g => !likedGenres.some(l => l.id === g.id)
+    );
+
+    const removed = likedGenres.filter(
+      g => !newValue.some(n => n.id === g.id)
+    );
+
+    for (const genre of added) {
+      await fetch(`/api/kirby/like-genre`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ genreId: genre.id }),
+      });
+    }
+
+    for (const genre of removed) {
+      await fetch(`/api/kirby/like-genre`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ genreId: genre.id }),
+      });
+    }
+
+    setLikedGenres(newValue);
+  };
   
   return (
     <Box
@@ -45,7 +93,9 @@ export default function Profile() {
           <Autocomplete
             multiple
             options={genres}
+            value={likedGenres}
             getOptionLabel={(genre) => genre.name}
+            onChange={handleChange}
             renderInput={(params) => (
               <TextField
                 variant="outlined"
--- packages/server/routes/genreRoutes.ts
+++ packages/server/routes/genreRoutes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { getGenres, likeGenre } from "../controllers/genreController";
+import { getGenres, getLikedGenres, likeGenre } from "../controllers/genreController";
 
 const router = Router();
 
 router.get("/genres", getGenres);
+router.get("/liked-genres", getLikedGenres);
 router.post("/like-genre", likeGenre);
 
 export default router;
