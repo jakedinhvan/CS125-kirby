835f8eabde155c92e6ac07687f98026f2d5fda29
Sarah Son <sonse@uci.edu>
2026-03-19 16:36:14 -0700

    fixed floating genre tags on AnimeCard

--- packages/client/src/components/AnimeCard.tsx
+++ packages/client/src/components/AnimeCard.tsx
@@ -1,4 +1,4 @@
-import { Card, CardActions, CardContent, Chip, IconButton, Link, Stack, Typography } from "@mui/material";
+import { Box, Card, CardActions, CardContent, Chip, IconButton, Link, Stack, Typography } from "@mui/material";
 import type { Anime } from "@kirby/types";
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import FavoriteIcon from '@mui/icons-material/Favorite';
@@ -14,7 +14,13 @@ const AnimeCard = ({
 }) => {
   return (
     <Card variant="outlined" sx={{ width: "100%", height: "100%", display: "flex", flexDirection: "column", position: "relative" }}>
-      <CardContent>
+      <CardContent
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          height: "100%",
+        }}
+      >
         <Typography gutterBottom sx={{ color: 'text.secondary', fontSize: 14 }}>
           {anime.seasonYear}
         </Typography>
@@ -22,24 +28,34 @@ const AnimeCard = ({
           <Link href={`/anime/${anime.id}`}>{anime.title}</Link>
         </Typography>
 
-        <Typography variant="body2"
-          sx={{
-            display: "-webkit-box",
-            WebkitLineClamp: 3,
-            WebkitBoxOrient: "vertical",
-            overflow: "hidden",
-            textOverflow: "ellipsis",
-            mb: 1,
-            minHeight: "4.5em",
-          }}
-          dangerouslySetInnerHTML={{ __html: anime.description ?? "" }}
-        />
+        <Box sx={{ flexGrow: 1 }}>
+          <Typography
+            variant="body2"
+            sx={{
+              display: "-webkit-box",
+              WebkitLineClamp: 3,
+              WebkitBoxOrient: "vertical",
+              overflow: "hidden",
+              textOverflow: "ellipsis",
+              mb: 1,
+              minHeight: "4.5em",
+            }}
+            dangerouslySetInnerHTML={{ __html: anime.description ?? "" }}
+          />
+        </Box>
 
-        <Stack direction="row" spacing={1} flexWrap="wrap">
+        <Box
+          sx={{
+            display: "flex",
+            flexWrap: "wrap",
+            gap: 1,
+            mt: "auto",
+          }}
+        >
           {anime.genres?.map((genre) => (
             <Chip key={genre} label={genre} variant="outlined" color="primary" />
           ))}
-        </Stack>
+        </Box>
         
       </CardContent>
           <IconButton
